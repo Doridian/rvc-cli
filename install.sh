3200879956fef6c40caeeca862beb944ca878669
Doridian <git@doridian.net>
2025-03-14 15:59:08 -0700

    simplify

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+set -ex
+
 uv run rvc_cli.py prerequisites --pretraineds_v1_f0=false --pretraineds_v1_nof0=false --pretraineds_v2_f0=false --pretraineds_v2_f0=false --exe=false
+
+uv run rvc_cli.py download --model_link "file://$(realpath ./logs/CABAL.zip)"
--- say.sh
+++ say.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-set -e
+set -ex
 
-set -x
-rm -f ./logs/*.flac
+rm -vf ./logs/*.flac
 uv run rvc_cli.py tts --tts_file '' --tts_text "$1" --tts_voice 'en-US-GuyNeural' --output_tts_path ./logs/tts.flac --output_rvc_path ./logs/rvc.flac --pth_path ./logs/CABAL/CABAL.pth --index_path ./logs/CABAL/model.index
 ffplay -autoexit ./logs/rvc.flac
